1c18f7b2828ae893b1218582b7e0d56d4176bbcd
gaoweifeng <gao_weifeng2@uxsino.com>
2021-06-10 15:08:13 +0800

    update

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,26 +1,43 @@
 <template>
   <div>
-    <el-input v-model="userName" type="text"  placeholder="用户名" ></el-input>
-    <h2>{{userName}}</h2>
+    <el-input v-model="username" type="text"  placeholder="用户名" ></el-input>
+    <h2>{{username}}</h2>
     <el-input v-model="password" type="text"  placeholder="密码"></el-input>
-      <h2>{{password}}</h2>
+    <h2>{{password}}</h2>
     <el-row>
-    <el-button type="primary">login</el-button>
-      <el-button type="primary">login</el-button>
+    <el-button type="primary" @click="requetLogin">login</el-button>
     </el-row>
   </div>
 </template>
 
 <script>
-
+import { mapActions } from 'vuex'
 export default {
   data() {
     return {
-      userName: '',
-      password: ''
+      username: 'username',
+      password: '123456'
+    }
+  },
+  methods: {
+    ...mapActions([
+      'handleLogin',
+      'getUserInfo'
+    ]),
+    login() {
+      this.handleLogin(this.data).then(res => {
+        this.getUserInfo().then(res => {
+          // eslint-disable-next-line standard/object-curly-even-spacing
+          this.$router.push({ name: 'Home'})
+        })
+        // eslint-disable-next-line handle-callback-err
+      }).catch(error => {
+        console.log(2222)
+      })
     }
   }
 }
 </script>
+
 <style>
 </style>
--- src/main.js
+++ src/main.js
@@ -5,16 +5,17 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
-
 import './style/theme.css'
 import './style/character.css'
-
+import axios from 'axios'
+import QS from 'qs'
 // 注册element-ui
 Vue.use(ElementUI)
-
+// 引入axios，并加到原型链中
+Vue.prototype.$axios = axios
+Vue.prototype.qs = QS
 Vue.config.productionTip = false
-
-/* eslint-disable no-new */
+// eslint-disable-next-line no-new
 new Vue({
   el: '#app1',
   router,
