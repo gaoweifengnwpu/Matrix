01ed05689d185dab07640a1ddc45dfb58c19c5ca
gaoweifeng <gao_weifeng2@uxsino.com>
2021-06-10 15:20:42 +0800

    update

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -5,39 +5,20 @@
     <el-input v-model="password" type="text"  placeholder="密码"></el-input>
     <h2>{{password}}</h2>
     <el-row>
-    <el-button type="primary" @click="requetLogin">login</el-button>
+    <el-button type="primary"  onclick="say('hi')">login</el-button>
     </el-row>
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
 export default {
   data() {
     return {
-      username: 'username',
-      password: '123456'
-    }
-  },
-  methods: {
-    ...mapActions([
-      'handleLogin',
-      'getUserInfo'
-    ]),
-    login() {
-      this.handleLogin(this.data).then(res => {
-        this.getUserInfo().then(res => {
-          // eslint-disable-next-line standard/object-curly-even-spacing
-          this.$router.push({ name: 'Home'})
-        })
-        // eslint-disable-next-line handle-callback-err
-      }).catch(error => {
-        console.log(2222)
-      })
+      username: '',
+      password: ''
     }
   }
 }
 </script>
-
 <style>
 </style>
